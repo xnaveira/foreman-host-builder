5d3826f7d6257686408488b271dfcc7d0046bfd4
Xavier Naveira <xnaveira@gmail.com>
2014-10-23 12:26:06 +0200

    Version 1.14

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ except ImportError:
     exit(1)
 
 setup(name='foreman-host-builder',
-      version='1.13',
+      version='1.14',
       description='Build hosts in Foreman from a template file',
       author='Xavier Naveira',
       author_email='xnaveira@gmail.com',
